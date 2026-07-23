89dc91a784a6380310965fbbfb8342697cdb45c0
Aditya R. Jawanjal <97746325+AdiJawanjal@users.noreply.github.com>
2026-07-24 00:22:51 +0530

    Fix string formatting in concatenation.py

--- concatenation.py
+++ concatenation.py
@@ -6,3 +6,7 @@ print("my age is", age)
 #O/P will be "my age is 24"
 
 print("Hello, Python Language \n I am \"good\"", "I'm ", age, "years old")
+
+''' It will print this 
+"Hello, Python Language 
+ I am "good" I'm  24 years old" '''
