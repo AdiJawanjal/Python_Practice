d3e82cc27918701e8f148cc2f30f10b2aee6b2b2
Aditya R. Jawanjal <97746325+AdiJawanjal@users.noreply.github.com>
2026-07-24 20:39:21 +0530

    Add loop examples and break statement in loop.py
    Added examples for for loop, while loop, and break statement.

--- loop.py
+++ loop.py
@@ -1,14 +1,24 @@
+# For Loop
 for i in range(1, 6)
     print((i))
 # The range is 1 to n-1 which is 6-1 or 5, This will print 1 2 3 4 5
 
 #---------------------------------------------------------------------------
 
-#While Loop
+# While Loop
 i = 1
 while (i<6):
     print(i)
     i += 1
 # This will print 1 to 5
 
+#----------------------------------------------------------------------------
+# Break
+i = 1
+while (i<6):
+    print(i)
+    i += 1
+    if i == 3:
+        break
+# This will stop executing at 2 so it will only print 1 2
 #-----------------------------------------------------------------------------
