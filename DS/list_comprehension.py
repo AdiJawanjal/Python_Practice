e97ceb8adac9b0e4b551ab9234c4edd6e37f8c47
Aditya R. Jawanjal <97746325+AdiJawanjal@users.noreply.github.com>
2026-08-02 17:33:51 +0530

    Refactor table creation using append method
    Refactored list comprehension to use append method.

---
+++ DS/list_comprehension.py
@@ -0,0 +1,21 @@
+table=[]
+for i in range(1,11):
+    table.append(5*i)
+
+
+print(table)
+
+table=[i*5 for i in range(1,11)]
+print(table)
+
+'''
+table=[]
+for i in range(1,11):
+    table.append(5*i)
+
+
+print(table)
+
+table=[i*5 for i in range(1,11)]
+print(table)
+'''
